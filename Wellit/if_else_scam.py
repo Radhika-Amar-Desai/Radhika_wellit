0a5b3678ef10ad96b569b1780314dfc350d09537
Radhika-Amar-Desai <120047754+Radhika-Amar-Desai@users.noreply.github.com>
2023-09-22 14:14:31 +0530

    Update if_else_scam.py

--- Wellit/if_else_scam.py
+++ Wellit/if_else_scam.py
@@ -1,34 +1,34 @@
 import pandas as pd
 
 # Define the name you want to search for
-search_name = "DASUK"
+search_name = "BArora"
 
 # Load the CSV file into a DataFrame
-file_path = "SIH - Sheet1.csv"  # Replace with the actual file path
+file_path = "RadhikaData1.csv"  # Replace with the actual file path
 df = pd.read_csv(file_path)
 
+df['VILLAGE'] = df['VILLAGE'].str.upper()
 # Search for the name in the "village" column
-result = df[df['Village'] == search_name]
+result = df[df['VILLAGE'] == search_name.upper()]
 
 # Check if any matches were found
 if not result.empty:
-    # Assuming the depth column is named "depth," you can change this if needed
-    depth_value = result.iloc[0]['Depth']
+    depth_value = result.iloc[0]['DEPTH']
 
-    may_decade_bgl = result.iloc[0]['May_decade_bgl']
-    aug_decade_bgl = result.iloc[0]['Aug_decade_bgl']
-    nov_decade_bgl = result.iloc[0]['Nov_decade_bgl']
-    jan_decade_bgl = result.iloc[0]['Jan_decade_bgl']
+    may_bgl = result.iloc[0]['MAY_bgl']
+    aug_bgl = result.iloc[0]['AUG_bgl']
+    nov_bgl = result.iloc[0]['NOV_bgl']
+    jan_bgl = result.iloc[0]['JAN_bgl']
 
-    may_bgl = result.iloc[0]['May_bgl']
-    aug_bgl = result.iloc[0]['Aug_bgl']
-    nov_bgl = result.iloc[0]['Nov_bgl']
-    jan_bgl = result.iloc[0]['Jan_bgl']
+    may_rise = result.iloc[0]['MAY_flu']
+    aug_rise = result.iloc[0]['AUG_flu']
+    nov_rise = result.iloc[0]['NOV_flu']
+    jan_rise = result.iloc[0]['JAN_flu']
 
-    may_rise = result.iloc[0]['May_rise']
-    aug_rise = result.iloc[0]['Aug_rise']
-    nov_rise = result.iloc[0]['Nov_rise']
-    jan_rise = result.iloc[0]['Jan_rise']
+    if sum ( [ may_rise , aug_rise , nov_rise , jan_rise ] ) < 0:
+        print ( "Well cannot be constructed." )
+    else:
+        print ( "Well can be constructed." )  
 
 else:
-    print(f"No entry found for {search_name} in the 'village' column.")
+    print ( "Oops Village not found." )
